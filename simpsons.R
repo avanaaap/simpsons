ff7ba46f5668e26ca24bafdd4654b96f473fd49c
“pavana” <pavanaupadh1@gmail.com>
2023-05-22 11:27:53 -0700

    added trendline

--- simpsons.R
+++ simpsons.R
@@ -12,10 +12,14 @@ simpsons <-
 #ggplot(data = simpsons, mapping = aes(x = season, y = guest_star)) + geom_bar(stat ='identity', width = 0.5) +theme(axis.text.x = element_text(angle = 65, vjust = 0.6)) +labs(title = "Histogram on Categorical Variable",subtitle = "Manufacturer across Vehicle Classes")
 
 #ggplot(simpsons, aes(x = guest_star)) +
+
 #geom_histogram(stat="count")
- order(simpsons$season)
+
+##+ geom_vline(aes(xintercept=mean(season)),color="blue", linetype="solid", linewidth=1) 
+head(simpsons$season)
+or <- order(simpsons$season)
 g <- aggregate(guest_star ~ season,data = simpsons, FUN = length)
 
-ggplot(data=g,mapping = aes(x = season, y = guest_star)) + geom_histogram(stat ='identity') + labs(title = "Histogram on Amount of Guest Stars per season") + geom_vline(aes(xintercept=mean(season)),
-                                                                                                                                                                       color="blue", linetype="solid", linewidth=1) 
-wa
+ggplot(data=g,mapping = aes(x = factor(season,level=or), y = guest_star)) + geom_bar(stat ='identity') + labs(title = "Bar plot on amount of guest stars per season") + xlab("season number") + ylab("number of guest stars")
+
+
